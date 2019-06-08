5e3236de0bca5dbb497481d39eb4598a1dab46af
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-06-08 19:53:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,12 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
-  return object
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, object);
+  delete newObject.key;
+
+  return newObject;
 }
